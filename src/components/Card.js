2f50fd62358008e280b714ea2b99bfb192094aa7
ahstevens11 <austin.cards20@gmail.com>
2021-05-23 10:41:27 -0500

    finished DeckBuilder

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,56 @@
 import React from 'react';
 
+import './Card.css';
+
 const Card = ({
-    name,
-    type,
-    manaCost,
-    text,
-    flavor,
-    imageUrl
+  id,
+  name,
+  type,
+  manaCost,
+  text,
+  flavor,
+  imageUrl,
+  addCardToDeck,
+  removeCardFromDeck
 }) => {
-    return(
-        <div className="Card">
-            { name } - { manaCost } <br />
-            { type } <br />
-            { text } <br />
-            { flavor } <br />
-            <img src={ imageUrl } />
-        </div>
-    )
+  return (
+    <div className="Card">
+      <div className="info">
+        <p className="header">
+          { name } - { manaCost }
+        </p>
+        <p className="type">
+          { type }
+        </p>
+        <p className="text">
+          { text }
+        </p>
+        <p className="flavor">
+          { flavor }
+        </p>
+      </div>
+      <img className="preview" src={ imageUrl } />
+      <div className="actions">
+        <button
+          onClick={() => {
+            addCardToDeck({
+              id,
+              name
+            })
+          }}>
+            (+) Add to Deck
+          </button>
+        <button
+          onClick={() => {
+            removeCardFromDeck({
+              id
+            })
+          }}>
+          (-) Remove from Deck
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
-import Card from './Card'
+import Card from './Card';
+import './SearchResults.css';
 
-const SearchResults = ({results}) => {  
- return (
+const SearchResults = ({ 
+  results,
+  addCardToDeck,
+  removeCardFromDeck
+}) => {
+  return (
     <div id="results">
-        <h3>Here's what we found ({ results.length } results):</h3>
+      <h3>Here's what we found ({ results.length } results):</h3>
+      <div className="CardList">
         {
-            results.map(result => (
-                <Card key={ result.id } { ...result } />
-            ))
+          results.map(result => (
+            <Card 
+              key={ result.id } 
+              addCardToDeck={ addCardToDeck }
+              removeCardFromDeck={ removeCardFromDeck }
+              {...result} />
+          ))
         }
+      </div>
     </div>
   );
 }
